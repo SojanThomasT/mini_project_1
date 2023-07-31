6fe6ee6ef695d3cf52316fa82298cd54341909bd
SojanThomasT <tsojanthomas@gmail.com>
2023-07-31 13:59:08 +0530

    4th edit

--- project.py
+++ project.py
@@ -8,10 +8,10 @@ def roll():
   return roll
 
 while True:
-  players = input("Number of players (2-4")
-  if players.isdigit("):
+  players = input("Number of players (2-4): ")
+  if players.isdigit():
       players = int(players)
-      if 1<=players<=4:
+      if 2<=players<=4:
         break
       else:
         print("Must be between 2 - 4 players")
